abeb5dba3586c98c460aa478f8365004c386ea2c
Jacob Thompson <jacobalthompson@gmail.com>
2026-01-08 18:54:51 -0800

    add 35

---
+++ 35.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <print>
+#include <vector>
+
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target)
+    {
+        auto it = std::lower_bound(nums.begin(), nums.end(), target);
+        return static_cast<int>(std::distance(nums.begin(), it));
+    }
+};
+
+int main()
+{
+    auto sol = new Solution;
+
+    std::vector<int> nums = {1, 3, 5, 6};
+    std::vector<int> targs = {5, 2, 7};
+
+    std::print("nums = [");
+    for (auto const &num : nums)
+        std::print("{}, ", num);
+    std::println("\b\b]");
+
+    for (auto const &target : targs)
+        std::println("target = {}; output = {}", target, sol->searchInsert(nums, target));
+
+    return 0;
+}
